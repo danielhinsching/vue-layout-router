4fe5f592e86df5e916b5f7113baab09715067f22
danielhinsching <danielsantoshinsching@gmail.com>
2023-07-28 12:50:12 -0300

    sdsa

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,36 @@
-<script setup></script>
-
+<script setup>
+  import AboutView from "./views/AboutView.vue"
+  import HomeView from "./views/HomeView.vue"
+</script>
 <template>
   <header>HEADER</header>
-  <aside>MENU</aside>
-  <main>MAIN</main>
+  <aside>
+    <nav class="menu">
+    <router-link to="/" class="home" >Home</router-link>
+    <br>
+     <router-link to="about" class="about">Sobre</router-link>     
+
+
+    </nav>
+  </aside>
+  <main>
+  <router-view/>
+  </main>
   <footer>FOOTER</footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+.menu{
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;}
+.home{
+background-color: cadetblue;
+display: flex
+
+
+}
+.about{
+background-color: cadetblue;  
+}
+</style>
